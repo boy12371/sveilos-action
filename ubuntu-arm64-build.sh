8c13ad748e724783683ff2ca1efe82fbaed836ac
boy12371 <boy12371@hotmail.com>
2021-04-25 22:27:31 +0800

    20210425214615

--- ubuntu-arm64-build.sh
+++ ubuntu-arm64-build.sh
@@ -7,10 +7,10 @@ apt-get -y install build-essential asciidoc gawk gettext git libncurses5-dev \
   libelf-dev autoconf libtool autopoint device-tree-compiler antlr3 gperf \
   curl swig rsync apt-transport-https golang-go libncursesw5-dev \
   libsepol1-dev checkpolicy policycoreutils selinux-basics dialog
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
 BIN_SH="$(ls -la /bin/sh)"
